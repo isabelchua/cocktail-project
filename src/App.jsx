217b9978986a2c0b9f019e58864d44b504ef802c
isabelchua <isabelchua86@gmail.com>
2023-10-25 17:46:48 -0700

    display list of cocktails

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import "./App.css";
+import { cocktailData } from "./data/cocktailData";
 
 function App() {
 	return (
-		<>
-			<div>
-				<h1>Cocktails</h1>
-				<li></li>
-			</div>
-		</>
+		<div>
+			<h1>List of Cocktails</h1>
+			<ul>
+				{cocktailData.map((cocktail, index) => (
+					<li key={index}>
+						<h2>{cocktail.name}</h2>
+						<p>Category: {cocktail.category}</p>
+						<p>Glass: {cocktail.glass}</p>
+						<p>Ingredients:</p>
+						<ul>
+							{cocktail.ingredients.map((ingredient, i) => (
+								<li key={i}>
+									{ingredient.amount} {ingredient.unit}{" "}
+									{ingredient.ingredient}
+								</li>
+							))}
+						</ul>
+						<p>Garnish: {cocktail.garnish}</p>
+						<p>Preparation: {cocktail.preparation}</p>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 }
 
