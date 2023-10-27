641b2e691a5c40e0720a099789ce23b99bfbcf6c
isabelchua <isabelchua86@gmail.com>
2023-10-26 18:46:43 -0700

    cocktail component

---
+++ src/data/cocktailData copy.js
@@ -0,0 +1,13 @@
+export const cocktailData = [
+	{
+		name: "Takoyaki",
+
+		category: "Snack",
+		ingredients: [
+			{ unit: "g", amount: 6, ingredient: "Flour" },
+			{ unit: "g", amount: 1.5, ingredient: "Eggs" },
+			{ unit: "cl", amount: 0.75, ingredient: "Water" }
+		],
+		preparation: ""
+	}
+];
