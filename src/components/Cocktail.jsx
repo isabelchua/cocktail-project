7666bf3b10b9839b3477ad3c4dfbbcd5915728f1
isabelchua <isabelchua86@gmail.com>
2023-10-31 09:25:33 -0700

    moved cocktail component content

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -1,5 +1,27 @@
+import { cocktailData } from "./../data/cocktailData";
+
 function Cocktail() {
-	return <div>Cocktail</div>;
+	return (
+		<ul>
+			{cocktailData.map((cocktail, index) => (
+				<li key={index}>
+					<h2>{cocktail.name}</h2>
+					<p>Glass: {cocktail.glass}</p>
+					<p>Ingredients:</p>
+					<ul>
+						{cocktail.ingredients.map((ingredient, i) => (
+							<li key={i}>
+								{ingredient.amount} {ingredient.unit}{" "}
+								{ingredient.ingredient}
+							</li>
+						))}
+					</ul>
+					<p>Garnish: {cocktail.garnish}</p>
+					<p>Preparation: {cocktail.preparation}</p>
+				</li>
+			))}
+		</ul>
+	);
 }
 
 export default Cocktail;
