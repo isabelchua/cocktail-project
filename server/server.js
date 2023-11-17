1cb288ea4e95a75268126d15d370504933584450
isabelchua <isabelchua86@gmail.com>
2023-11-17 09:47:08 -0800

    server running

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
-// const express = require("express");
-// const fetch = require("node-fetch");\
+const express = require("express");
+// const fetch = require("node-fetch");
+const fetch = require("node-fetch").default;
 
-import fetch from "node-fetch";
-import express from "express";
+// import fetch from "node-fetch";
+// import express from "express";
+
+require("dotenv").config();
 
 const app = express();
 const port = 3001;
-const apiKey = import.meta.env.VITE_REACT_APP_IMAGE_KEY;
+// const apiKey = import.meta.env.VITE_REACT_APP_IMAGE_KEY;
+const apiKey = process.env.VITE_REACT_APP_IMAGE_KEY;
 
 app.use(express.json());
 
@@ -26,3 +30,27 @@ app.get("/search", async (req, res) => {
 app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
 });
+
+app.get("/", (req, res) => {
+	res.send("Hello, this is your server!");
+});
+
+app.get("/search", async (req, res) => {
+	const apiKey = process.env.VITE_REACT_APP_IMAGE_KEY;
+	const serpApiUrl = `https://serpapi.com/search.json?q=margarita&hl=en&gl=us&engine=google_images&api_key=${apiKey}&location=United%20States`;
+
+	try {
+		const response = await fetch(serpApiUrl);
+		const data = await response.json();
+		res.json(data);
+	} catch (error) {
+		console.error("Error proxying request:", error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
+
+/////
